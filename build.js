b17d21d643f75364539e122712a7ac88e3e05fb2
Sergey Beryozkin <sberyozkin@gmail.com>
2019-01-17 13:35:49 +0000

    Adding the info about 2.3.0.Final and 2.3.1.Final-SNAPSHOT

--- build.js
+++ build.js
@@ -148,8 +148,9 @@ function build() {
       '/docs/2-1-0-Final': 'http://docs.thorntail.io/2.1.0.Final',
       '/docs/2-2-0-Final': 'http://docs.thorntail.io/2.2.0.Final',
       '/docs/2-2-1-Final': 'http://docs.thorntail.io/2.2.1.Final',
+      '/docs/2-3-0-Final': 'http://docs.thorntail.io/2.3.0.Final',
       '/docs/4-x': 'http://docs.thorntail.io/4.0.0-SNAPSHOT',
-      '/docs/HEAD': 'http://docs.thorntail.io/2.3.0.Final-SNAPSHOT',
+      '/docs/HEAD': 'http://docs.thorntail.io/2.3.1.Final-SNAPSHOT',
 }))
 
 
